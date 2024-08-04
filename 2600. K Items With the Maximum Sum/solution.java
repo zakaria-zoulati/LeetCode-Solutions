6872ca1b0b550787c80ec0a6b3efa6bce7a143ff
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-04 22:29:08 +0200

    done

--- 2600. K Items With the Maximum Sum/solution.java
+++ 2600. K Items With the Maximum Sum/solution.java
@@ -5,31 +5,7 @@ class Solution {
         }else if( k<= numOnes + numZeros ){
             return numOnes ; 
         }else{
-            int curr = 0 ; 
-            int rs = 0 ; 
-            while( k - curr > numOnes + numZeros ){
-                numNegOnes-- ; 
-                curr++ ; 
-                rs-- ; 
-            }
-            while( curr < k && numOnes>0 ){
-                rs++ ; 
-                numOnes-- ; 
-                curr++ ; 
-            }
-
-            if( curr<k  ){
-                if( curr + numZeros >= k  ){
-                    return rs ; 
-                }else{
-                    curr += numZeros ; 
-                }
-            }
-            
-            return rs ; 
-
-
+            return  2*numOnes + numZeros - k ; 
         }
-
     }
 }
