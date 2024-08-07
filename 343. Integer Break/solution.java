3f8bb9e9f184b0f7a0d65f03489ca8a8c1646f7d
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-07 16:55:27 +0200

    done

---
+++ 343. Integer Break/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int integerBreak(int n) {
+        if( n==2 ) return 1  ;
+        if( n==3 ) return 2 ; 
+        int[] dp = new int[n+1] ; 
+        dp[2] = 2 ; 
+        dp[1] = 1  ;
+        dp[3] = 3 ; 
+        for(int i=3 ; i<=n ; ++i){
+            for(int j=1 ; j<=i/2  ; ++j){
+                dp[i] = Math.max( dp[i-j]*dp[j],dp[i] ) ; 
+            }
+        }
+        return dp[n] ; 
+    }
+}
