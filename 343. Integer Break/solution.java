c3c44b7e5b573a9b3475a83eb72d54035e3cb99d
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-13 00:53:39 +0200

    done

--- 343. Integer Break/solution.java
+++ 343. Integer Break/solution.java
@@ -1,16 +1,18 @@
 class Solution {
-    public int integerBreak(int n) {
-        if( n==2 ) return 1  ;
-        if( n==3 ) return 2 ; 
-        int[] dp = new int[n+1] ; 
-        dp[2] = 2 ; 
-        dp[1] = 1  ;
-        dp[3] = 3 ; 
-        for(int i=3 ; i<=n ; ++i){
-            for(int j=2 ; j<=i/2  ; ++j){
-                dp[i] = Math.max( dp[i-j]*dp[j],dp[i] ) ; 
-            }
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() >= t.length()) {
+            return s.equals(t);
         }
-        return dp[n] ; 
+        
+        byte a = 0;
+        int i=0 ; 
+        while(i<t.length() && a!=s.length()){
+            if(t.charAt(i)==s.charAt(a)){
+                a++ ;
+            }
+            i++ ;
+
+        }
+       return a == s.length(); 
     }
 }
