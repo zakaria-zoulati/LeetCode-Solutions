a78e39912bf3baf06f80de4a4669a6ac14e89f83
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-13 19:52:41 +0200

    done

---
+++ 300. Longest Increasing Subsequence/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int max  = (int) 1e5 ; 
+        max++ ; 
+        int dif = (int) 1e4 ; 
+        int n = nums.length ; 
+        ArrayList<Integer> arr = new ArrayList<>() ; 
+        int rs = 0 ;
+        int[] dp = new int[max] ; 
+        for(int i=0 ; i<n ; ++i){
+            nums[i] += dif ; 
+        }
+        for(int i=0 ; i<n ; ++i){
+            int pos = 0 ; 
+            pos = Collections.binarySearch(arr, nums[i] );
+            if( !arr.contains( nums[i] )){
+                if (pos < 0) {
+                    pos = -(pos + 1);  
+                }
+                if (pos < arr.size()) {
+                    arr.set(pos, nums[i] ); 
+                } else {
+                    arr.add( nums[i] );  
+                }
+            }
+            int current = 0 ; 
+            for(int j = pos-1 ; j>=0 ; --j ){
+                current = Math.max( dp[  arr.get(j)  ] , current ) ;
+            }
+            current++ ; 
+            dp[nums[i]] = Math.max(  dp[nums[i]] , current ) ; 
+            rs = Math.max( rs , dp[nums[i]] ) ; 
+        }
+        return rs ; 
+    }
+}
