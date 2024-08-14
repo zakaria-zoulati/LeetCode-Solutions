06059f44aa98b7b2e31f27d1f7cfc1ec5e07a74d
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-14 04:16:53 +0200

    done

--- 318. Maximum Product of Word Lengths/solution.java
+++ 318. Maximum Product of Word Lengths/solution.java
@@ -3,11 +3,10 @@ class Solution {
         int n = words.length  ; 
         int[] arr = new int[n] ; 
         int rs  = 0  ; 
-        for(int i=0 ; i<n  ; ++i){
-           for(int j=0 ; j<words[i].length(); ++j ){
-                arr[i] |= 1 << ( words[i].charAt(j) - 'a' ) ; 
-           }
-        }
+        for (int i=0; i<n; i++)
+            for (char c: words[i].toCharArray())
+                    arr[i] |= (1 << (c - 'a'));
+                    
         for(int i=0 ;i<n-1 ; ++i){
             for(int j=i+1 ; j<n ; ++j){
                 if( ( arr[i] & arr[j] ) == 0 ){
