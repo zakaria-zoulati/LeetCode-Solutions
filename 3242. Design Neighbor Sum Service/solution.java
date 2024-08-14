c62591b7ca888f59b73d9cf6037fa8efe1ad51cd
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-14 23:38:19 +0200

    solution

--- 3242. Design Neighbor Sum Service/solution.java
+++ 3242. Design Neighbor Sum Service/solution.java
@@ -1,65 +1,61 @@
-class neighborSum {
+class NeighborSum {
     int[][] grid ; 
     int n = 0 ;
-    public neighborSum(int[][] grid) {
+    int[] adj  ;
+    int[] diag ;
+    public NeighborSum(int[][] grid) {
         this.grid = grid ; 
         this.n = grid.length ; 
+        adj = new int[n*n]; 
+        diag =  new int[n*n] ; 
+        compute( grid , adj , diag ) ; 
     }
-    
-    public int adjacentSum(int value) {
-        int rs = 0 ; 
-        for(int i=0 ; i<n ; i++){
-            for(int j=0; j<n ; j++){
-                if( grid[i][j] == value ){
-                    if( i+1<n  ){
-                        rs += grid[i+1][j] ; 
+    public void compute( int[][] grid  , int[] adj , int[] diag ){
+        for(int i=0 ; i<n ; ++i){
+            for(int j=0 ; j<n ; j++ ){
+                boolean up = i-1 >=0 ; 
+                boolean down = i+1 <n ; 
+                boolean right = j+1 <n ; 
+                boolean left = j-1 >=0 ; 
+                // Updates The Adjacent values map
+                if( down ){
+                   adj[ grid[i][j] ] +=  grid[i+1][j]  ;    
+                }
+                if( up ){
+                    adj[ grid[i][j] ] +=  grid[i+-1][j]  ; 
+                }
+                if( right  ){
+                    adj[ grid[i][j] ] +=  grid[i][j+1]  ; 
+                }
+                if( left ){ 
+                    adj[ grid[i][j] ] +=  grid[i][j-1]  ; 
+                }
+                // Updates The diagonal values map 
+                if( up ){
+                        if(left){  
+                            diag[ grid[i][j] ] += grid[i-1][j-1] ; 
+                        }
+                        if(right ){ 
+                             diag[ grid[i][j] ] += grid[i-1][j+1] ; 
+                        }
                     }
-                    if( i-1 >=0 ){
-                        rs += grid[i-1][j] ; 
-                    }
-                     if( j+1<n  ){
-                        rs += grid[i][j+1] ; 
-                    }
-                    if( j-1 >=0 ){
-                        rs += grid[i][j-1] ; 
-                    }
-                    return rs ; 
+                if( down ){
+                        if(left){
+                             diag[ grid[i][j] ] += grid[i+1][j-1] ; 
+                        }
+                        if( right ){
+                             diag[ grid[i][j] ] += grid[i+1][j+1] ; 
+                        }
                 }
             }
         }
-        return -1  ;
+    }
+    public int adjacentSum(int value) {
+        return adj[value]  ; 
     }
     
     public int diagonalSum(int value) {
-        int rs = 0 ;
-        for(int i=0; i<n ; i++){
-            for(int j=0; j<n ; j++){
-                if( grid[i][j] == value ){
-                    boolean up = i-1 >=0 ; 
-                    boolean down = i+1 <n ; 
-                    boolean right = j+1 <n ; 
-                    boolean left = j-1 >=0 ; 
-                    if( up ){
-                        if(left){
-                            rs += grid[i-1][j-1] ; 
-                        }
-                        if(right ){
-                            rs += grid[i-1][j+1] ; 
-                        }
-                    }
-                    if( down ){
-                        if(left){
-                            rs += grid[i+1][j-1] ; 
-                        }
-                        if( right ){
-                            rs += grid[i+1][j+1] ; 
-                        }
-                    }
-                    return rs ; 
-                }
-            }
-        }
-        return -1 ; 
+        return diag[value] ; 
     }
 }
 
