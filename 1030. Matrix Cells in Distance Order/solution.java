3f92c5fa7e2caddde512eb0518c65a509c2d4e71
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-15 17:57:40 +0200

    done

--- 1030. Matrix Cells in Distance Order/solution.java
+++ 1030. Matrix Cells in Distance Order/solution.java
@@ -1,21 +1,26 @@
-import java.util.*;
-
 class Solution {
     public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
         int len = rows * cols;
         int[][] rs = new int[len][];
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter) - Math.abs(b[0] - rCenter) - Math.abs(b[1] - cCenter)
-        );
 
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                pq.add(new int[]{i, j});
+        int[][] directions  = new int[][]{ {0,-1} , {-1,0} , {1,0} , {0,1} } ; 
+        Queue< int[] > q = new LinkedList<>() ;
+        boolean[][] seen = new boolean[rows][cols] ; 
+        q.add(  new int[]{ rCenter , cCenter}  ) ; 
+        seen[rCenter][cCenter] = true ; 
+        int i = 0 ; 
+        while( !q.isEmpty() ){
+            int[] curr = q.remove() ; 
+            rs[i++] = curr ; 
+            for(int[] d : directions  ){
+                int r = curr[0] + d[0] ; 
+                int c = curr[1] + d[1] ; 
+                if( r>=0 && r<rows && c>=0 && c<cols && !seen[r][c] ){
+                    seen[r][c] = true ; 
+                    q.add(  new int[]{r,c} ) ; 
+                }
             }
         }
-        for (int i = 0; i < len; ++i) {
-            rs[i] = pq.poll();
-        }
-        return rs;
+        return rs ; 
     }
 }
