7029601a08c6c95f41966882a745d7e9ad3e5600
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-22 04:40:55 +0200

    done

---
+++ 476. Number Complement/solution2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findComplement(int num) {
+       String inter = String.valueOf( num ) ;
+       int a = 0 ; 
+       int b = num ;
+       while( b!=0  ){
+            b >>= 1 ;
+            a++ ;
+       }
+       return ( (1<<a) - num  ) -1  ; 
+    }
+}
