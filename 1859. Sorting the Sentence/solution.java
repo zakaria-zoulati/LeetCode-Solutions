9285f91513651201c481ebd28925153ef9c30c4f
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-28 16:01:32 +0200

    done

---
+++ 1859. Sorting the Sentence/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void swap( int i , int j , String[] all ){
+        String temp = all[i] ; 
+        all[i] = all[j] ; 
+        all[j] = temp ;  
+    }   
+    public String sortSentence(String s) {
+        String[] all = s.split(" ") ; 
+        int n = all.length ; 
+        for(int j=0 ; j<2; ++j){
+            for( int i=0 ; i<n ; ++i ){
+                swap(  i ,  all[i].charAt( all[i].length() -1 ) - '1' , all  ) ; 
+            }
+        }
+        
+        StringBuilder rs = new StringBuilder() ; 
+        for( String item : all ){
+            rs.append(  item.substring( 0 , item.length() -1  )) ; 
+            rs.append( " " ) ; 
+        }
+
+        return rs.toString().trim() ; 
+
+    }
+}
