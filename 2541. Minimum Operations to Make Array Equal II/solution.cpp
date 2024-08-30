3079d6d72e5b3cdab2497c218379fbcc993dfdae
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-08-31 00:24:39 +0200

    done

---
+++ 2541. Minimum Operations to Make Array Equal II/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    long long minOperations(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size() ; 
+        if( k==0 ){
+            for(int i=0 ; i<n ; ++i){
+                if( nums1[i] != nums2[i]  ) return -1 ; 
+            }
+            return 0 ; 
+        }
+        long long rs = 0 ; 
+        long long inter = 0 ; 
+        for(int i=0 ; i<n ; ++i){
+            int diff = nums1[i] - nums2[i] ; 
+            if( diff % k != 0  ) return -1 ;  
+            inter += diff/k ; 
+            rs += abs(  diff/k ) ; 
+        }
+
+        if( inter == 0  ){
+            return rs/2  ;
+        }
+
+        return -1 ; 
+
+    }
+};
