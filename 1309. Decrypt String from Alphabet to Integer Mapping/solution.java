8f4b11d68e2257710fc89627f648bb32edfc36ac
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-01 03:17:12 +0200

    done

--- 1309. Decrypt String from Alphabet to Integer Mapping/solution.java
+++ 1309. Decrypt String from Alphabet to Integer Mapping/solution.java
@@ -2,10 +2,9 @@ class Solution {
     public String freqAlphabets(String s) {
         StringBuilder rs = new StringBuilder(); 
         int n = s.length(); 
-        
         for (int i = 0; i < n; ++i) {
             if (i + 2 < n && s.charAt(i + 2) == '#') {
-                int num = Integer.parseInt(s.substring(i, i + 2)); 
+                int num = (s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0'); 
                 rs.append((char) (num - 1 + 'a')); 
                 i += 2; 
             } else {
