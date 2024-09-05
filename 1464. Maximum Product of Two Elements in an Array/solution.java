953f0d7f7c0247a66709a63b4f98bf824f423667
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-05 15:08:01 +0200

    done

---
+++ 1464. Maximum Product of Two Elements in an Array/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        Arrays.sort( nums ) ; 
+        int n = nums.length ; 
+        return ( nums[n-1] - 1 )*( nums[n-2] - 1 ) ; 
+ 
+    }
+}
