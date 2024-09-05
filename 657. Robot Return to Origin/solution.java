07975e63cff60d3eba9c3e63a066a26f9ef7b33d
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-05 14:12:07 +0200

    done

---
+++ 657. Robot Return to Origin/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0 ; 
+        int y = 0 ; 
+        for( char c : moves.toCharArray() ){
+            if( c == 'U' ) y++ ; 
+            else if( c == 'D' ) y-- ; 
+            else if( c == 'R' ) x++ ; 
+            else if( c == 'L' ) x-- ; 
+        }
+        return x==0 && y==0 ; 
+    }
+}
