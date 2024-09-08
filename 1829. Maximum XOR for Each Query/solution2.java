c884430cc0a8280d3e2e9daad284ad1fd950b749
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-08 19:27:55 +0200

    done

---
+++ 1829. Maximum XOR for Each Query/solution2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] getMaximumXor(int[] nums, int maximumBit) {
+        int n = nums.length ; 
+        int all = 0 ; 
+        for(int i=1 ; i<n ; ++i){
+            nums[i] ^= nums[i-1] ; 
+        }
+        int right = n-1 ; 
+        int left = 0; 
+        while( left < right ){
+            int temp = nums[right] ; 
+            nums[right] = nums[left] ; 
+            nums[left] = temp ; 
+            left++ ; 
+            right-- ; 
+        }
+        int mask = ( 1 << maximumBit ) - 1; 
+        for(int i=0 ; i<n ; ++i){
+            nums[i] ^= mask ; 
+        }
+        return nums ; 
+    }
+}
