c04dd5c1d317b0eba905eea6acd8940531e4d56e
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-10 03:07:57 +0200

    done

---
+++ 974. Subarray Sums Divisible by K/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int n = nums.length ; 
+        HashMap<Integer , Integer> map = new HashMap<>() ; 
+        int rs = 0 ;
+        nums[0] = ( 10000*k + nums[0] ) % k  ; 
+        if( nums[0]  == 0  ){
+            rs++ ; 
+            map.put( 0 , 2 ) ; 
+        }else {
+            map.put( 0 , 1 ) ; 
+            map.put( nums[0] , 1 ) ;
+        }
+        for(int i=1 ; i<n ; ++i){
+            nums[i] = ( nums[i] + nums[i-1] + 10000*k ) % k ; 
+            int count = map.getOrDefault( nums[i] , 0 ) ;
+            rs += count ;
+            map.put( nums[i] , count + 1 ) ;
+        }
+        return rs ; 
+    }
+}
