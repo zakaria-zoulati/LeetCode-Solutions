e724bb7e691613e391b247bbd7914dbab6697152
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-11 22:49:26 +0200

    done

---
+++ 1393. Capital Gain/Loss/solution.sql
@@ -0,0 +1,5 @@
+SELECT stock_name,
+       SUM(CASE WHEN operation = 'Sell' THEN price ELSE 0 END) -
+       SUM(CASE WHEN operation = 'Buy' THEN price ELSE 0 END) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name;
