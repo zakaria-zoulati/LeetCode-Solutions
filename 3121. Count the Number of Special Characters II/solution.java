6181adf6aed82f1fcaae1ddd83ae6163c0862c8b
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-11 05:29:42 +0200

    done

--- 3121. Count the Number of Special Characters II/solution.java
+++ 3121. Count the Number of Special Characters II/solution.java
@@ -4,13 +4,12 @@ class Solution {
         String min = "abcdefghijklmnopqrstuvwxyz" ; 
         String maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ; 
         for(int i=0 ; i<26 ; ++i){
-            int a = word.indexOf( min.charAt(i) ) ; 
+            int a = word.lastIndexOf( min.charAt(i) ) ; 
             int b = word.indexOf( maj.charAt(i) ) ; 
             if( a != -1 &&  b != -1 ){
-                if( a < b && word.lastIndexOf( min.charAt(i) ) < b   ) rs++ ;
+                if( a < b   ) rs++ ;
             }
         }
         return rs ; 
-
     }
 }
