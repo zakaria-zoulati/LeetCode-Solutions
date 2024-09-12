3a19fc0773168f9469834267c8437236c7b221fb
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-12 18:01:51 +0200

    done

---
+++ 1015. Smallest Integer Divisible by K/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+       if( k % 2 == 0 || k % 5 == 0 ){
+            return -1 ; 
+       }
+       int len = 1 ; 
+       int curr = 1%k ; 
+       while( curr != 0 ){
+            curr = ( curr*10 + 1 )%k ; 
+            len++ ; 
+       } 
+       if( curr != 0 ){
+            return  -1 ; 
+       }
+       return len ; 
+    }
+}
