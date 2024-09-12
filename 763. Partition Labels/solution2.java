57d27cb1c60f021a073f723e89a060d3b3dbda13
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-12 23:17:59 +0200

    done

---
+++ 763. Partition Labels/solution2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int n = s.length() ; 
+        List<Integer> rs = new ArrayList<>() ;
+        int[] all = new int[26] ; 
+        int start = 0 ; 
+        int end = 0 ; 
+        for(int i=0 ; i<n ; ++i){
+            all[ s.charAt(i) - 'a' ] = i ; 
+        }
+        for(int i=0 ; i<n ; ++i){
+            end = Math.max( all[ s.charAt(i) - 'a' ] ,  end ) ; 
+            if( i == end ){
+                rs.add( end - start + 1 ) ; 
+                start = i+1 ; 
+            }
+        }
+        return rs ; 
+    }
+}
