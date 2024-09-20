be6a7fcb66b782766f248a4f89bbb506085bcc3e
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-20 04:21:02 +0200

    done

---
+++ 1835. Find XOR Sum of All Pairs Bitwise AND/solution2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getXORSum(int[] arr1, int[] arr2) {
+        int xor1 = 0 ; 
+        int xor2 = 0 ; 
+        for( int num : arr1 ){
+           xor1 ^= num  ; 
+        }
+        for( int num : arr2 ){
+           xor2 ^= num ; 
+        }
+        return xor1 & xor2 ; 
+    }
+}
