35d67b933d0899638e7f7c23b594a55ad38dca9f
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-25 19:03:21 +0200

    done

---
+++ 462. Minimum Moves to Equal Array Elements II/solution2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        int n = nums.length ; 
+        int rs = 0 ; 
+        Arrays.sort( nums ) ; 
+        int i = 0 ; int j = n-1 ; 
+        while( i<j ){
+            rs += nums[j] - nums[i] ; 
+            i++ ; 
+            j-- ; 
+        }
+        return rs ; 
+    }
+}
