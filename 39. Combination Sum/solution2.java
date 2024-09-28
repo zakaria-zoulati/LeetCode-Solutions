4cd6c6a5174117ddd47571b75fe7fda6e9587e31
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-09-28 11:24:01 +0200

    done

---
+++ 39. Combination Sum/solution2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void backtrack( int[] nums , int sum , int t  , List<List<Integer>> rs , List<Integer> curr ,  int index  ){
+        if( sum == t ){
+            rs.add( curr ) ; 
+            return  ; 
+        }
+        else if( sum > t ){
+            return ; 
+        }else {
+            for(int i = index ; i< nums.length ; ++i){
+                if( nums[i] + sum > t ) continue ; 
+                List<Integer> next = new ArrayList<>(curr) ; 
+                next.add( nums[i] ) ; 
+                backtrack( nums , sum + nums[i] , t , rs , next , i ) ; 
+            }
+        }
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>() ; 
+        for(int i = 0 ; i<candidates.length ; ++i ){
+            List<Integer> curr = new ArrayList<>() ; 
+            curr.add( candidates[i] ) ; 
+            backtrack( candidates , candidates[i] , target , list , curr , i  ) ; 
+        }
+        return list ; 
+    }
+}
