5f99c513dff579337086b77efb6e016ee11edbbf
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-10-04 22:17:34 +0200

    done

---
+++ 852. Peak Index in a Mountain Array/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length; 
+        int left = 0; 
+        int right = n - 1; 
+        while (left < right) {  
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;  
+            } else {
+                right = mid;  
+            }
+        }
+        return right ;  
+    }
+}
