1fd8d42349eb4b173678ae1fc502f20bdfb1036d
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-10-10 21:55:09 +0200

    done

---
+++ 477. Total Hamming Distance/solution4.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int totalHammingDistance(int[] a) {
+        int ans=0,n=a.length;
+        for(int i=0;i<32;i++) {
+            int c=0;
+            for(int j:a) c+=(j>>i)&1;
+            ans+=c*(n-c);
+        }
+        return ans;
+    }
+}
