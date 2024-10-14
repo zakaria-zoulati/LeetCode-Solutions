b88796a2b7d9d322b9b5e8571acf4019e19312b3
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-10-14 22:24:45 +0200

    done

---
+++ 2222. Number of Ways to Select Buildings/solution2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long numberOfWays(String s) {
+        int n = s.length() ; 
+        int[] dp = new int[n] ; 
+        if( s.charAt(0) == '0' ){
+            dp[0] = 1 ; 
+        }
+        for( int i=1 ; i<n ; ++i ){
+            dp[i] += dp[i-1] ; 
+            if( s.charAt(i) == '0' ) dp[i]++ ; 
+        }
+        long rs = 0L ; 
+        for( int i=1 ; i<n-1 ; ++i ){
+            if( s.charAt(i) == '0' ){
+                rs += (long) ( i - dp[i-1] )*( n-i-1 - ( dp[n-1] -  dp[i] ) ) ; 
+            }else {
+                rs += (long) dp[i-1]*( dp[n-1] - dp[i] ) ; 
+            }
+        }
+        return rs ; 
+    }
+}
