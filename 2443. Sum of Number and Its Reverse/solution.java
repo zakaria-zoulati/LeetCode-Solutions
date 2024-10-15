9880bd77b1f4340a0f422e0f7de3fe6bb4cd9880
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-10-15 22:07:48 +0200

    done

--- 2443. Sum of Number and Its Reverse/solution.java
+++ 2443. Sum of Number and Its Reverse/solution.java
@@ -10,7 +10,6 @@ class Solution {
     public boolean sumOfNumberAndReverse(int num) {
         for(int i=0 ; i<=num ; ++i){
             if( ( i + rev(i) ) == num ) {
-                System.out.println(i) ; 
                 return true ; 
             }
         }
