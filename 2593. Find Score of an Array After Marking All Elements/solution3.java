f58153b9e13cf339f3cafc9bcb668b538449aad5
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-10-31 13:23:13 +0100

    done

---
+++ 2593. Find Score of an Array After Marking All Elements/solution3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long findScore(int[] nums) {
+        long rs = 0; 
+        int n = nums.length;
+        boolean[] marked = new boolean[n]; 
+        Integer[] a = new Integer[n];
+        for(int i = 0; i < n; i++) {
+            a[i] = i; 
+        }
+        Arrays.sort(a, (x, y) -> Integer.compare(nums[x], nums[y]));
+        for (int i = 0; i < n; i++) {
+            int ind = a[i]; 
+            if (marked[ind]) continue;
+            rs += nums[ind];
+            marked[ind] = true; 
+            if (ind > 0) marked[ind - 1] = true; 
+            if (ind < n - 1) marked[ind + 1] = true; 
+        }
+        return rs;
+    }
+}
