b553087d8969f6a3be8c70871a4e202f8f0cbb36
Zakaria Zoulati <zoulatizakaria@gmail.com>
2024-11-02 11:04:38 +0100

    done

---
+++ 2490. Circular Sentence/solution2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isCircularSentence(String s) {
+        int n = s.length() ; 
+        if( s.charAt(0) != s.charAt(n-1) ) return false ; 
+        int k = s.indexOf(' ') ; 
+        if( k == -1 ){
+            return true ; 
+        }
+        while( k!=-1 ){
+            if( s.charAt(k-1) != s.charAt(k+1)) return false ; 
+            k = s.indexOf( ' ' , k+1 ) ; 
+        }   
+        return true ;
+    }
+}
