4e65d584f45127ef924545219c08b8b5f520548c
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-11-04 21:36:51 +0100

    done

--- 790. Domino and Tromino Tiling/solution.java
+++ 790. Domino and Tromino Tiling/solution.java
@@ -6,7 +6,6 @@ class Solution {
         dp[0] = 1 ; 
         dp[1] = 1 ; 
         dp[2] = 2 ;
-        dp[3] = 5 ; 
         for( int i = 3 ; i<=n ; ++i ){
            dp[i] = ( dp[i-1] + dp[i-2] ) % mod ;
            for( int j=i-1 ; j>=2 ; --j ){
