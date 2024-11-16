e5e3a2f326e3b99f246acd8e599ddcd45ee6a611
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-11-16 16:41:32 +0100

    done

--- 6. Zigzag Conversion/solution.java
+++ 6. Zigzag Conversion/solution.java
@@ -5,22 +5,21 @@ class Solution {
         StringBuilder rs = new StringBuilder() ; 
         char[][] grid = new char[numRows][n-n/3] ; 
         boolean flag = false ; 
-        int a = 0 ; 
         int b = 0 ; 
+        numRows-- ; 
         for( int i=0 ; i<n ; ++i ){
-            if( i%(numRows-1) == 0 ){
+            if( i%numRows == 0 ){
                 flag = !flag ; 
                 if( !flag ){
-                    grid[ numRows - 1 ][b] = s.charAt(i) ; 
+                    grid[ numRows ][b] = s.charAt(i) ; 
                     ++b ; 
                 }else {
                     grid[0][b] = s.charAt(i) ; 
                 } 
             }else if( flag ) {
-                grid[ i%(numRows-1) ][b] = s.charAt(i) ; 
+                grid[ i%numRows ][b] = s.charAt(i) ; 
             }else {
-                grid[numRows-1-(i%(numRows-1)) ][b] = s.charAt(i) ; 
-                ++b ; 
+                grid[numRows-i%numRows][b++] = s.charAt(i) ; 
             }
         }
         for( char[] l : grid ){
