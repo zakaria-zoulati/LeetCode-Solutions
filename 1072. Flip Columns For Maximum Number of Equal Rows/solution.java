2e18c4369f0c708f7bc87f461ff68e6b78cbfcaf
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-11-22 14:06:58 +0100

    done

---
+++ 1072. Flip Columns For Maximum Number of Equal Rows/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+        int m = matrix.length ; 
+        int n = matrix[0].length ; 
+        int rs = 0 ; 
+        int[] k = new int[n] ; 
+        boolean[] isSeen = new boolean[m] ; 
+        for( int i=0 ; i<m ; ++i ){
+            if( isSeen[i] ) continue ; 
+            isSeen[i] = true ; 
+            for(int j=0 ; j<n ; ++j){
+                k[j] = matrix[i][j] ^ 1 ; 
+            }
+            int co = 1 ;
+            for(int j=0 ; j<m ; ++j){
+                if( isSeen[j] ) continue ; 
+                boolean flag = true ; 
+                int val = matrix[j][0]^k[0] ; 
+                for( int t=1 ;t<n ; ++t ){
+                    if( (matrix[j][t] ^ k[t]) != val ){
+                        flag = false ; 
+                        break ; 
+                    }
+                }
+                if( flag ){
+                    ++co ; 
+                    isSeen[j] = true ;
+                }
+            }
+            if( co > rs ) rs = co ;  
+        }
+        return rs ; 
+    }
+}
