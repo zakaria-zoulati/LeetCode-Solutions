0d1651095429be459022a9bc5d92b7a376f6304d
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-11-24 10:54:05 +0100

    done

---
+++ 221. Maximal Square/solution.java
@@ -0,0 +1,44 @@
+class Solution {
+    int n ; 
+    int m ; 
+    public int maximalSquare(char[][] matrix) {
+        n = matrix.length ; 
+        m = matrix[0].length ; 
+        int rs = 0 ; 
+        int[][] sum = new int[n][m] ; 
+        for( int i=0 ; i<n ; ++i ){
+            if( matrix[i][0] == '1' ){
+                sum[i][0] = 1 ; 
+            }
+            for( int j=1 ; j<m ; ++j ){
+                sum[i][j] += sum[i][j-1] ; 
+                sum[i][j] += (matrix[i][j] - '0') ; 
+            }
+        }
+        for( int c=0 ; c<m ; ++c ){
+            for( int r=0 ; r<n-1; ++r ){
+                sum[r+1][c] += sum[r][c] ; 
+            }
+        }
+
+        for( int i=0 ; i<n ; ++i ){
+            for( int j=0 ; j<m ; ++j ){
+                int p = 0; 
+                while( isPossible( sum , i , j , p ) ){
+                    ++p ; 
+                    rs = Math.max( rs , p*p ) ; 
+                }
+            }
+        }
+        return rs ;
+    }
+
+    public boolean isPossible( int[][] sum ,int i , int j , int p ){
+        if( i-p>=0 && j-p>=0 ){
+            int diff = ( j-p>0 ? sum[i][j-p-1] : 0 ) + ( i-p>0 ? sum[i-p-1][j] : 0 ) - ( i-p>0 && j-p>0 ? sum[i-p-1][j-p-1] : 0 ) ; 
+            return (sum[i][j] - diff ) == (p+1)*(p+1) ; 
+        }
+        return false  ; 
+    }
+
+}
