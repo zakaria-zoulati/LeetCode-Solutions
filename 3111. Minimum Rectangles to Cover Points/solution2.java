2dd6d46676d131dba216c04925c26e52921564a0
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-01 11:12:02 +0100

    done

---
+++ 3111. Minimum Rectangles to Cover Points/solution2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minRectanglesToCoverPoints(int[][] points, int w) {
+        int n = points.length ; 
+        Arrays.sort( points , (a,b) -> a[0] - b[0] );
+        int rs = 0 ; 
+        int rightBound = -1 ; 
+        for( int[] i : points ){
+            if( i[0] > rightBound ){
+                rightBound = i[0] + w ; 
+                ++rs ; 
+            }
+        }
+        return rs ; 
+    }
+}
