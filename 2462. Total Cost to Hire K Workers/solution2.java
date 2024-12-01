9fa7d8d319faf5ed39d04f18dbacc49b1d1a6037
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-01 16:48:33 +0100

    done

---
+++ 2462. Total Cost to Hire K Workers/solution2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public long totalCost(int[] costs, int k, int candidates) {
+        int n = costs.length ; 
+        long rs = 0L ;
+        if( n == k ){
+            for( int i : costs ){
+                rs += i ; 
+            }
+            return rs ; 
+        } 
+        PriorityQueue<Integer> front = new PriorityQueue<>() ; 
+        PriorityQueue<Integer> last = new PriorityQueue<>() ; 
+        int f = Math.min(candidates-1,n-1) ;
+        for( int i=0 ; i<=f ; ++i ){
+            front.add( costs[i] ) ; 
+        } 
+        int l = Math.max( f+1 , n-candidates ) ; 
+        for( int i=l ; i<n ; ++i ){
+            last.add( costs[i] ) ; 
+        }
+        for( int i=0 ; i<k ; ++i ){
+            if( front.isEmpty() ){
+                rs += last.poll() ; 
+            }else if( last.isEmpty() ){
+                rs += front.poll() ; 
+            }else {
+                int a = front.peek() ; 
+                int b = last.peek() ; 
+                if( a <= b ){
+                    rs += front.poll() ; 
+                    if( f+1 < l ){
+                        front.add( costs[++f] ) ; 
+                    }
+                }else {
+                    rs += last.poll() ; 
+                    if( f < l -1 ){
+                        last.add( costs[--l] ) ; 
+                    }
+                }
+            }
+        }
+        return rs ; 
+    }
+}
