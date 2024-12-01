c2a9bc621fc139b20a48932b5215e1d80b25b52b
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-01 22:10:13 +0100

    done

--- 695. Max Area of Island/soution.java
+++ 695. Max Area of Island/soution.java
@@ -1,27 +1,27 @@
 class Solution {
-    int[][] directions = { {-1,0} , {1,0} , {0,1} , {0,-1} } ; 
+    int[][] dirs = { {-1,0},{1,0},{0,1},{0,-1} } ; 
     int n , m ; 
-    public int dfs( int[][] grid , int i , int j , boolean[][] isSeen ){
+    public int dfs( int[][] g , int i , int j , boolean[][] isSeen ){
         isSeen[i][j] = true ; 
         int curr = 1 ;
-        for( int[] d : directions ){
-            int x = i + d[0] ; 
-            int y = j + d[1] ; 
-            if( x>=0 && x<n && y>=0 && y<m && grid[x][y] == 1 && !isSeen[x][y] ){
-                curr += dfs( grid , x , y , isSeen ) ;
+        for( int[] d : dirs ){
+            int x = i+d[0]; 
+            int y = j+d[1]; 
+            if( x>=0 && x<n && y>=0 && y<m && g[x][y]==1 && !isSeen[x][y] ){
+                curr += dfs( g,x,y,isSeen ) ;
             }
         }
         return curr ; 
     } 
-    public int maxAreaOfIsland(int[][] grid) {
+    public int maxAreaOfIsland(int[][] g) {
         int rs = 0 ; 
-        n = grid.length ; 
-        m = grid[0].length ; 
+        n = g.length ; 
+        m = g[0].length ; 
         boolean[][] isSeen = new boolean[n][m] ; 
         for(int i=0 ; i<n ; ++i){
             for( int j=0 ; j<m ; ++j ){
-                if( grid[i][j] == 1 && !isSeen[i][j] ){
-                    rs = Math.max( rs , dfs( grid , i , j , isSeen ) ) ;
+                if( g[i][j]==1 && !isSeen[i][j] ){
+                    rs = Math.max( rs , dfs( g , i , j , isSeen )) ;
                 }
             }   
         }
