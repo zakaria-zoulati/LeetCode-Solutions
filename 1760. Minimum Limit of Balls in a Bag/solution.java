d8b2d016189d5f829745076391443eba9d8afa3c
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-07 11:14:25 +0100

    done

--- 1760. Minimum Limit of Balls in a Bag/solution.java
+++ 1760. Minimum Limit of Balls in a Bag/solution.java
@@ -5,10 +5,10 @@ class Solution {
         int rs = 0 ; 
         int left = 1 ; 
         int right = nums[n-1] ; 
-        while( left <= right ){
+        while( left < right ){
             int mid = left + ( right - left )/2 ; 
             if( isValid( nums , mid , ops ) ){
-                right = mid - 1 ; 
+                right = mid ; 
             }else {
                 left = mid + 1 ; 
             }
@@ -18,7 +18,7 @@ class Solution {
 
     public boolean isValid( int[] nums , int mid , int ops ){
         int curr = 0 ; 
-        for( int i=nums.length - 1 ; i>=0 ; --i ){
+        for( int i=nums.length - 1 ; i>=0 && curr<=ops ; --i ){
             if( nums[i] <= mid ) break ; 
             curr +=  nums[i]/mid + (nums[i]%mid==0?-1:0 ) ; 
         }
