5524058a24c3fc49787784eb4bb5998b61a323c7
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-08 14:50:01 +0100

    done

--- 2708. Maximum Strength of a Group/solution2.java
+++ 2708. Maximum Strength of a Group/solution2.java
@@ -24,3 +24,24 @@ class Solution {
         return nums[r] == 0 ? 0 : nums[l]; 
     }
 }
+class Solution {
+    public long maxStrength(int[] nums) {
+        int n = nums.length; 
+        Arrays.sort(nums); 
+        long rs = 1L; 
+        int l = 0; 
+        int r = n - 1; 
+        boolean f = false; 
+        while (r >= 0 && nums[r] > 0) {
+            rs *= nums[r--];  
+            f = true;  
+        }  
+        while (l + 1 <= r && nums[l + 1] < 0) {
+            rs *= nums[l++]; 
+            rs *= nums[l++]; 
+            f = true;
+        }
+        if( f ) return rs ; 
+        return nums[r] == 0 ? 0 : nums[l]; 
+    }
+}
