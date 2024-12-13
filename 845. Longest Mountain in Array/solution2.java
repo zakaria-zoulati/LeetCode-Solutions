ea1d908b73239281a301b64abec4871c9c21de57
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-13 22:18:29 +0100

    done

---
+++ 845. Longest Mountain in Array/solution2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestMountain(int[] nums) {
+        int n = nums.length ; 
+        if( n < 3 ) return 0 ; 
+        int rs = 0 ; 
+        for( int i=1 ; i<n ; ++i ){
+            int up = 0 ; 
+            while( i<n && nums[i] > nums[i-1] ){
+                ++i ; 
+                ++up ; 
+            }
+            if( up == 0 ) continue ; 
+            int dow = 0 ; 
+            while( i<n && nums[i] < nums[i-1] ){
+                ++i ; 
+                dow++; 
+            }
+            if( dow == 0 ) continue ; 
+            rs = Math.max( rs , up+dow+1 ) ; 
+            --i ; 
+        }
+        return rs ; 
+    }
+}
