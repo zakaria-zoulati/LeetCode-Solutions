8e4252c2622249df3712a05b1a397e40fc89b12f
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-15 16:36:25 +0100

    done

---
+++ 1424. Diagonal Traverse II/solution4.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        int count = 0;
+        int n = nums.size() ; 
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i <n ; ++i) {
+            List<Integer> row = nums.get(i);
+            for (int j = 0; j < row.size(); ++j) {
+                int idx = i + j;
+                if (lists.size() <= idx ) {
+                    lists.add(new ArrayList<>());
+                }
+                lists.get(idx).add(row.get(j));
+                count ++;
+            }
+        }
+        int[] res = new int[count];
+        int idx = 0;
+        for (List<Integer> list : lists) {
+            for (int i = list.size() - 1; i >= 0; --i) {
+                res[idx++] = list.get(i); 
+            }
+        }
+        return res;
+    }
+}
