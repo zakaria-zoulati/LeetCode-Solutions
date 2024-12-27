ac2912269ac8c46eb3d4286537f03994ae1a7efb
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-27 21:27:47 +0100

    done

--- 3113. Find the Number of Subarrays Where Boundary Elements Are Maximum/solution2.java
+++ 3113. Find the Number of Subarrays Where Boundary Elements Are Maximum/solution2.java
@@ -1,20 +1,20 @@
 class Solution {
     public long numberOfSubarrays(int[] nums) {
         int n = nums.length ; 
-        long rs = 1L ; ; 
+        long rs = 0L ; ; 
         int[][] s = new int[n][2] ; 
         int in = -1 ; 
-        s[++in] = new int[]{nums[0],1}; 
-        for( int i=1 ; i<n ; ++i ){
-            int[] curr = new int[]{ nums[i] , 1}; 
-            while(in != -1 && s[in][0] <= nums[i] ){
-                if( s[in][0]  == nums[i] ){
-                    curr[1] += s[in][1] ; 
-                }
-                --in ; 
+        for( int i : nums){
+            int[] curr = new int[]{i , 1}; 
+            while( in != -1 && s[in][0] < i ){
+               --in ;  
             }
-            s[++in] = curr ; 
-            rs += curr[1] ; 
+            if( in == -1 || s[in][0] > i ){
+                s[++in] = curr ; 
+            }else {
+                s[in][1]++ ; 
+            }
+            rs += s[in][1] ; 
         }
         return rs ;  
     }
