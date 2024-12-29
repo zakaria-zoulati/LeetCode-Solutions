54fa086b0fdf2a8c8874826f0a36e20974efa899
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-29 12:38:17 +0100

    done

--- 1639. Number of Ways to Form a Target String Given a Dictionary/solution.java
+++ 1639. Number of Ways to Form a Target String Given a Dictionary/solution.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int numWays(String[] words, String target) {
-        int n = target.length() ;
+    public int numWays(String[] words, String t) {
+        int n = t.length() ;
         int m = words.length ; 
         int len = words[0].length() ; 
         int mod = 1_000_000_007 ; 
         int[][] f = new int[len][26];
-        // filling the f array 
+        // filling the frequency array of the i column of all words  
         for( String in : words ){
             for( int i=0 ; i<len ; ++i ){
-                f[i][ in.charAt(i) - 'a' ]++ ; 
+                ++f[i][in.charAt(i)-'a'] ; 
             }
         }
         long[][] dp = new long[n+1][len+1] ;
@@ -17,11 +17,10 @@ class Solution {
         } 
         // The number dp[i][j] represents the number of target( 0 , i ) till The len = j in words 
         for( int i=1 ; i<=n ; ++i ){
-            for( int j=1 ; j<=len ; ++j ){
-                dp[i][j] = (  dp[i][j-1] + f[j-1][ target.charAt(i-1) - 'a' ]*dp[i-1][j-1] )% mod ; 
+            for( int j=i ; j<=len ; ++j ){
+                dp[i][j] = ( dp[i][j-1] + f[j-1][t.charAt(i-1) - 'a']*dp[i-1][j-1] )% mod ; 
             }
         }
-
         return (int) dp[n][len] ; 
     }
 }
