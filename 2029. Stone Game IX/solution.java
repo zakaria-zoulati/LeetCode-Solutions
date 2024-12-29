2b0d3998be6b1f82c2340c852bf7d56fa570b741
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-30 00:57:48 +0100

    done

---
+++ 2029. Stone Game IX/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int n = stones.length ; 
+        int[] m = new int[3] ;
+        for( int i : stones ){
+            ++m[i%3] ; 
+        }
+        if( m[1] == 0 && m[2] == 0 ){
+            return false  ; 
+        }
+        int curr = 0 ; 
+        if( m[1] > 0 ){  
+            curr = Math.min( m[1]-1 , m[2] ) ; 
+            if( m[1] - curr > 2 ){
+               if( m[0] % 2 == 1 ){
+                return true ; 
+               } 
+            }else if( m[2] > curr ) {
+                if( m[0] % 2 == 0 ){
+                    return true ; 
+                }
+            }
+        }
+        if( m[2] > 0 ){
+            curr = Math.min( m[2] - 1 , m[1] ) ; 
+            if( m[1] == curr && m[2] - curr == 1 ){
+                return false ; 
+            }else if( m[2] - curr > 2 && m[0] % 2 == 1){
+                    return true ; 
+            }else if ( m[1] > curr && m[0] % 2 == 0  ) {
+                    return true ; 
+            }
+        }
+        return false  ;
+    }
+}
