6fb901d17cb61a3b5ae0ffa884f265f28debacf7
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-12-30 22:52:12 +0100

    done

--- 3403. Find the Lexicographically Largest String From the Box I/solution3.java
+++ 3403. Find the Lexicographically Largest String From the Box I/solution3.java
@@ -1,17 +1,21 @@
 class Solution {
     public String answerString(String word, int m) {
-        if(m==1) return word;
-        int n = word.length();
-        int maxlen = (n-m+1);
-        char max = word.charAt(0);
-        String res = "";
-        for(int i=0; i<n; i++) {
-            if(word.charAt(i) >= max) {
-                String curr = word.substring(i, Math.min(i+maxlen, n));
-                if(curr.compareTo(res) > 0) res = curr;
-                max = word.charAt(i);
+        if(m == 1) return word;
+        char max = ' ';
+        String ans = "";
+        for( char c : word.toCharArray() ){
+            if( c > max ){
+                max = c ; 
             }
         }
-        return res;
+        for(int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) == max) {
+                String temp = word.substring(i, Math.min(word.length(), i + word.length() - (m - 1)));
+                if(temp.compareTo(ans) > 0) {
+                    ans = temp;
+                }
+            }
+        }
+        return ans;
     }
 }
