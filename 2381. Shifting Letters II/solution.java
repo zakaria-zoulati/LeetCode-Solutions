33679fafe126a2f04081a74eceb4aeb58bbcba2f
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2025-01-05 16:17:05 +0100

    done

---
+++ 2381. Shifting Letters II/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int n = s.length() ; 
+        int m = shifts.length ; // The number of queries
+        char[] chars = s.toCharArray() ;
+        int[] diff = new int[n+1] ;
+        diff[0] = chars[0] - 'a' ; 
+        for( int i = 1 ; i<n ; ++i ){
+            diff[i] = chars[i] - chars[i-1] ; 
+        } 
+        for( int[] q : shifts ){
+            int l = q[0] ; 
+            int r = q[1] ; 
+            int in = q[2]==1?1:-1 ; 
+            diff[l] += in ; 
+            diff[r+1] -= in ; 
+        }
+        int dec = 10_000*26 ; 
+        // Rebuild The diffrence array after construction 
+        chars[0] = (char) ( 'a' + ( diff[0] + dec )%26 ) ; 
+        for( int i=1 ; i<n ; ++i ){
+            diff[i] += diff[i-1] ; 
+            chars[i] = (char) ( 'a' + ( diff[i] + dec )%26 ) ; 
+        }
+        return new String(chars) ;
+    }
+}
