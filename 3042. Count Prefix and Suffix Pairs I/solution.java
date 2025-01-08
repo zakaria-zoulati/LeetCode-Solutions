061d3163d6b6d1481810624045d1f9a7899faf78
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2025-01-08 10:07:02 +0100

    done

--- 3042. Count Prefix and Suffix Pairs I/solution.java
+++ 3042. Count Prefix and Suffix Pairs I/solution.java
@@ -4,7 +4,7 @@ class Solution {
         int rs = 0 ; 
         for( int i=0 ; i<n ; ++i ){
             for( int j=i+1 ; j<n ; ++j ){
-                if( words[j].indexOf( words[i]) == 0 && words[j].lastIndexOf( words[i]) == words[j].length() - words[i].length()){
+                if( words[j].startsWith( words[i]) && words[j].endsWith( words[i]) ){
                     ++rs ; 
                 }
             }
