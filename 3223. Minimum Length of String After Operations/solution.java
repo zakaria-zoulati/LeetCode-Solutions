2136b9d8d529f8af81edbda95d6c5751e425b3b7
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2025-01-13 15:52:12 +0100

    done

--- 3223. Minimum Length of String After Operations/solution.java
+++ 3223. Minimum Length of String After Operations/solution.java
@@ -1,16 +1,13 @@
 class Solution {
     public int minimumLength(String s) {
         int[] f = new int[128] ; 
-        for( char c : s.toCharArray() ){
+        for(char c : s.toCharArray()){
             ++f[c] ; 
         }
         int rs = 0 ; 
-        for( int i : f ){
-            if( i >= 3 ){
-                rs += 1 + (i%2==0?1:0) ;
-            }else {
-                rs += i ; 
-            }
+        for(int i:f){
+            if(i==0)continue ; 
+            rs += (i+1)%2 + 1 ; 
         }
         return rs  ;
     }
