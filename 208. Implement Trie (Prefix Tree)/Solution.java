ce990d994ebce83b603b05519449a46d3462f96d
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-01-30 18:40:08 +0100

    Trie Implementation

--- 208. Implement Trie (Prefix Tree)/Solution.java
+++ 208. Implement Trie (Prefix Tree)/Solution.java
@@ -53,11 +53,3 @@ class Trie {
         return childs[i].search( word , in+1 ) ;
     }
 }
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
