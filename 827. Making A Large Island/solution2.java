97e942bcce5733535761e1e60f0e3f279871df31
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-01-31 13:03:52 +0100

    Graph Pb

---
+++ 827. Making A Large Island/solution2.java
@@ -0,0 +1,62 @@
+class Solution {
+    int[] count ; 
+    int n ;
+    public int largestIsland(int[][] grid) {
+        this.n = grid.length ; 
+        int in = 2 ; 
+        int[][] rep = new int[n][n] ;
+        count = new int[n*n+3] ;  
+        for( int i=0 ; i<n ; ++i ){
+            for( int j=0 ; j<n ; ++j ){
+                dfs( i , j , grid , rep , in );
+                ++in ; 
+            }
+        }
+        int rs = 0 ; 
+        for( int i=2 ; i<in ; ++i ){
+            rs = Math.max( rs , count[i] ) ; 
+        }
+        boolean[] seen = new boolean[in];
+        int up, down, left, right, merge;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) {
+                    up = i > 0 ? grid[i - 1][j] : 0;
+                    down = i + 1 < n ? grid[i + 1][j] : 0;
+                    left = j > 0 ? grid[i][j - 1] : 0;
+                    right = j + 1 < n ? grid[i][j + 1] : 0;
+                    seen[up] = true;
+                    merge = 1 + count[up];
+                    if (!seen[down]) {
+                        merge += count[down];
+                        seen[down] = true;
+                    }
+                    if (!seen[left]) {
+                        merge += count[left];
+                        seen[left] = true;
+                    }
+                    if (!seen[right]) {
+                        merge += count[right];
+                        seen[right] = true;
+                    }
+                    rs = Math.max(rs, merge);
+                    seen[up] = false;
+                    seen[down] = false;
+                    seen[left] = false;
+                    seen[right] = false;
+                }
+            }
+        }
+        return rs;
+    }
+    public void dfs( int i , int j , int[][] g , int[][] rep , int in ){
+        if (i < 0 || i == n || j < 0 || j == n || g[i][j] != 1 || rep[i][j] > 0 ) return;
+        g[i][j] = in ; 
+        rep[i][j] = in ; 
+        count[in]++ ; 
+        dfs( i + 1, j , g , rep , in);
+        dfs( i - 1, j , g , rep , in);
+        dfs(i, j - 1 , g , rep , in);
+        dfs(i, j + 1 , g , rep , in);
+    }
+}
