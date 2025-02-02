64a22e4151cc26ab778bb2123694b253d7c0a853
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-02-02 14:43:16 +0100

    Greedy Pb

--- 3443. Maximum Manhattan Distance After K Changes/Solution.java
+++ 3443. Maximum Manhattan Distance After K Changes/Solution.java
@@ -1,31 +1,16 @@
 class Solution {
     public int maxDistance(String s, int k) {
-        int x = 0 ; 
-        int y = 0 ; 
-        int man_y = 0; 
-        int man_x = 0 ; 
-        int rs = 0 ; 
-        for( char c : s.toCharArray()){
-            if( c == 'E' ){
-                ++x ; 
-                ++man_x ; 
-            }else if( c == 'W' ){
-                --x ;
-                ++man_x ; 
-            }else if( c == 'N' ){
-                ++y ; 
-                ++man_y ; 
-            }else {
-                --y ; 
-                ++man_y ; 
-            }
-            int curr = Math.abs(x) + Math.abs(y) ; 
-            int moves = (man_x + man_y - curr )/2 ; 
-            curr += 2*Math.min( moves , k ) ; 
-            if( curr > rs ){
-                rs = curr ; 
-            }
+        int x = 0, y = 0, rs = 0, steps = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'E') x++;
+            else if (c == 'W') x--;
+            else if (c == 'N') y++;
+            else y--;
+            steps++;
+            int currDist = Math.abs(x) + Math.abs(y);
+            currDist += 2*Math.min((steps - currDist) >> 1, k); 
+            rs = Math.max(rs, currDist);
         }
-        return rs ; 
+        return rs;
     }
 }
