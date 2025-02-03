4473ba0b99021e168593fd676e63428c7bbdae49
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-02-03 08:46:35 +0100

    done

---
+++ 3105. Longest Strictly Increasing or Strictly Decreasing Subarray/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int n = nums.length ; 
+        int rs = 1 ; 
+        for( int i=0 ; i+1<n ;  ){
+            int curr = 1 ; 
+            if( nums[i] == nums[i+1] ) ++i ; 
+            else if ( nums[i+1] > nums[i] ){
+                while( i+1<n && nums[i+1] > nums[i] ){
+                    ++i ; 
+                    ++curr ; 
+                }
+            }else {
+                while( i+1<n && nums[i+1] < nums[i] ){
+                    ++i ; 
+                    ++curr ; 
+                }
+            }
+            rs = Math.max( rs , curr ) ; 
+        }
+        return rs ; 
+    }
+}
