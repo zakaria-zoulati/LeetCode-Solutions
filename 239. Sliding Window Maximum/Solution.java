05e705e5f4634373a0c83b98e9e70a4f0b66092a
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-03-06 15:37:46 +0000

    Dequeue Use Optimisation

--- 239. Sliding Window Maximum/Solution.java
+++ 239. Sliding Window Maximum/Solution.java
@@ -2,23 +2,23 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length ; 
         int[] rs = new int[n-k+1] ; 
-        Deque<int[]> dequeue = new LinkedList<>();
+        Deque<Integer> dequeue = new LinkedList<>();
         for( int i=0 ; i<k ; ++i ){
-            while( !dequeue.isEmpty() && dequeue.peekLast()[0] < nums[i] ){
+            while( !dequeue.isEmpty() && nums[dequeue.peekLast() ] < nums[i] ){
                 dequeue.removeLast() ; 
             }
-            dequeue.addLast( new int[] { nums[i] , i } ) ; 
+            dequeue.addLast( i ) ; 
         }
-        rs[0] = dequeue.peekFirst()[0] ; 
+        rs[0] = nums[ dequeue.peekFirst() ] ; 
         for( int i=k ; i<n ; ++i ){
-            while( !dequeue.isEmpty() && dequeue.peekFirst()[1] <= i-k  ){
+            while( !dequeue.isEmpty() && dequeue.peekFirst()  <= i-k  ){
                 dequeue.removeFirst() ; 
             }
-            while( !dequeue.isEmpty() && dequeue.peekLast()[0] < nums[i] ){
+            while( !dequeue.isEmpty() && nums[ dequeue.peekLast() ] < nums[i] ){
                 dequeue.removeLast() ; 
             }
-            dequeue.addLast( new int[]{ nums[i] , i } ) ;
-            rs[i-k+1] = dequeue.peekFirst()[0] ; 
+            dequeue.addLast( i ) ;
+            rs[i-k+1] =  nums[ dequeue.peekFirst() ] ; 
         }
         return rs ;
     }
