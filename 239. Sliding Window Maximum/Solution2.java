28e579ab75297bd2dc413967c5dab5b8014f132c
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-03-06 15:41:05 +0000

    No Buily-in DS Used

--- 239. Sliding Window Maximum/Solution2.java
+++ 239. Sliding Window Maximum/Solution2.java
@@ -2,25 +2,25 @@ class Solution2 {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length ; 
         int[] rs = new int[n-k+1] ; 
-        int[][] deq = new int[n+1][2]; 
+        int[] deq = new int[n+1]; 
         int start = -1 ; 
         int end = -1 ;  
         for( int i=0 ; i<k ; ++i ){
-            while( start != end  && deq[end][0] < nums[i] ){; 
+            while( start != end  && nums[ deq[end] ] < nums[i] ){; 
                 end-- ; 
             }
-            deq[++end] = new int[] { nums[i] , i } ; 
+            deq[++end] = i ; 
         } 
-        rs[0] = deq[ start+1 ][0] ; 
+        rs[0] = nums[ deq[ start+1 ] ] ; 
         for( int i=k ; i<n ; ++i ){
-            while( start != end && deq[start+1][1] <= i-k  ){
+            while( start != end && deq[start+1] <= i-k  ){
                 start++ ; 
             }
-            while( start != end && deq[end][0] < nums[i] ){
+            while( start != end && nums[ deq[end] ] < nums[i] ){
                 end-- ; 
             }
-            deq[++end] = new int[] { nums[i] , i } ; 
-            rs[i-k+1] = deq[ start+1 ][0] ; 
+            deq[++end] = i ;
+            rs[i-k+1] = nums [deq[ start+1 ] ] ; 
         }
         return rs ;
     }
