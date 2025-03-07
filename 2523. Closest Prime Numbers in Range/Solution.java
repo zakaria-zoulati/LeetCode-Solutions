54681cbfaf672a405f4d42ccb5625c7ffe8c9172
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-03-07 02:31:22 +0000

    A transaction optimisation

--- 2523. Closest Prime Numbers in Range/Solution.java
+++ 2523. Closest Prime Numbers in Range/Solution.java
@@ -1,12 +1,18 @@
 class Solution {
     public static boolean notPrime[] = new boolean[1_000_001] ; 
+    public static int[] transaction = new int[ 1_000_001 ] ; 
     static {
+        Arrays.fill( transaction , 1_000_001 ) ; 
+        notPrime[1] = true ; 
         for( int i=4 ; i<=1_000_001 ; i+=2 ){
             notPrime[i] = true ; 
         }
-        for( int i=3 ; (long) i*i<1_000_001 ; i += 2 ){
+        int prev = 2 ; 
+        for( int i=3 ; (long) i<1_000_001 ; i += 2 ){
             if( notPrime[i] ) continue ; 
-            for( int j = i*i ; j<1_000_001 ; j += i ){
+            transaction[ prev ] = i - prev ; 
+            prev = i ; 
+            for( long j = (long) i*i ; j<1_000_001 ; j += i ){
                 notPrime[ (int) j] = true ; 
             }
         }
@@ -15,23 +21,27 @@ class Solution {
         int[] rs = new int[]{ -1 , -1  } ; 
         int curr = Integer.MAX_VALUE ; 
         int prev = -1 ;
-        if( left <= 2 ){
+        if( left <=  2 ){
             prev = 2 ; 
-            left = 2 ; 
-        }
-        if( left % 2 == 0 ){
-            left++ ; 
-        }
-        while( left <= right  ){
-            if( !notPrime[left] ){
-                if( prev != -1 && left - prev < curr ){
-                    rs[0] = prev ; 
-                    rs[1] = left ; 
-                    curr = left - prev ; 
-                }
-                prev = left ; 
+            left = 3 ; 
+        }else {
+            if( left % 2 == 0 ) left++ ; 
+            while( left<right && notPrime[left] ){
+                left += 2 ; 
             }
-            left += 2 ; 
+            prev = left ; 
+            if( left >= right || notPrime[left] ) return rs ;
+            left += transaction[left] ; 
+        }
+
+        while( left <= right  ){
+            if( (left - prev) < curr ){
+                rs[0] = prev ; 
+                rs[1] = left ; 
+                curr = left - prev ; 
+            }
+            prev = left ; 
+            left += transaction[left] ; 
         }
 
         return rs ;
