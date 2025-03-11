f1d9c7c1f030afdb70ce2206278e950e98eaee49
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-03-11 11:10:24 +0000

    Sliding window Pb

---
+++ 1358. Number of Substrings Containing All Three Characters/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int n = s.length() ; 
+        int[] f = new int[128] ; 
+        int rs = 0 ;
+        int left = 0 ; 
+        int cdt = 0 ; 
+        for( int i = 0 ; i<n ; ++i ){ 
+            f[s.charAt(i)]++ ; 
+            cdt |= ( 1 << ( s.charAt(i) - 'a' ) ) ; 
+            if( cdt == 7 ){
+                while( f[ s.charAt(left) ] > 1 ){
+                    f[s.charAt(left++) ]-- ; 
+                }
+                rs += left+1 ; 
+            }
+        }
+        return rs ; 
+    }
+}
