8390c7068070f3a95095d7ed5fd57b44812ae38b
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-03-20 22:27:39 +0000

    A more better Solution

--- 2560. House Robber IV/Solution.java
+++ 2560. House Robber IV/Solution.java
@@ -1,34 +1,22 @@
 class Solution {
-    public boolean isPossible( int[] nums , int tar , int k ){
-        int n = nums.length ; 
-        // Try to get the longest subsequence that contains elments below the target
-        int[] dp = new int[n] ; 
-        int max = 0 ; 
-        for( int i=0 ; i<n ; ++i ){
-            if( nums[i] <= tar ){
-                dp[i] = 1 ; 
-                if( i >= 2  ){
-                    dp[i] = 1 + dp[i-2] ; 
-                }
-                max = Math.max( dp[i] , max ) ; 
-            }else if( i >= 1 ) {
-                dp[i] = dp[i-1] ; 
-            }   
-        }
-        return max >= k ; 
-    }
     public int minCapability(int[] nums, int k) {
         int n = nums.length ; 
-        int l = 1 ; 
-        int r = 1_000_000_000 ; 
-        int max = -1 ; 
+        int l = Integer.MAX_VALUE ; 
+        int r = -1 ; 
         for( int i : nums ){
-            max = Math.max( max , i ) ; 
+            r = Math.max( r , i ) ; 
+            l = Math.min( l , i ) ; 
         }
-        r = max ; 
         while( l < r ){
             int mid = l + ( r - l )/2 ; 
-            if( isPossible( nums , mid , k ) ){
+            int count = 0  ; 
+            for( int i=0 ; i<n ; ++i ){
+                if( nums[i] <= mid ){
+                    i++ ; 
+                    count++ ;
+                }
+            }
+            if(  count >= k ){
                 r = mid ; 
             }else {
                 l = mid+1 ; 
