907bec072d6a4a16d7e883908d560ece48bc34be
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-03-26 21:00:57 +0000

    A faster Solution

--- 329. Longest Increasing Path in a Matrix/Solution.java
+++ 329. Longest Increasing Path in a Matrix/Solution.java
@@ -1,38 +1,41 @@
 class Solution {
     int n , m ; 
-    static int[][] dirs = { {-1,0} , {1,0} , {0,1} , {0,-1} } ; 
-    public int dfs( int[][] matrix , int[][] isVisited , int[][] count , int i , int j ){
-        if( isVisited[i][j] == 1 ){
+    int rs ; 
+    boolean[][] isVisited ; 
+    public int dfs( int[][] matrix , int[][] count , int i , int j ){
+        if( isVisited[i][j] ){
             return count[i][j] ; 
         }
-        for( int[] d : dirs ){
-            int x = i + d[0] ; 
-            int y = j + d[1] ; 
-            if( x>=0 && y>=0 && x<n && y<m && matrix[x][y] > matrix[i][j] ){
-                count[i][j] = Math.max( count[i][j] , dfs( matrix , isVisited , count , x , y ) ) ; 
-            }
+        int t = i+1 ; 
+        if( t < n && matrix[t][j] > matrix[i][j] ){
+            count[i][j] = Math.max( count[i][j] , dfs( matrix , count , t , j ) ) ; 
+        }
+        t = i-1 ; 
+        if( t >= 0 && matrix[t][j] > matrix[i][j] ){
+            count[i][j] = Math.max( count[i][j] , dfs( matrix , count , t , j ) ) ; 
+        }
+        t = j+1 ; 
+        if( t < m && matrix[i][t] > matrix[i][j] ){
+            count[i][j] = Math.max( count[i][j] , dfs( matrix , count , i , t ) ) ; 
+        }
+        t = j-1 ; 
+        if( t >= 0 && matrix[i][t] > matrix[i][j] ){
+            count[i][j] = Math.max( count[i][j] , dfs( matrix , count , i , t ) ) ; 
         }
         count[i][j]++ ; 
-        isVisited[i][j] = 1 ; 
+        isVisited[i][j] = true ; 
         return count[i][j] ; 
     } 
     public int longestIncreasingPath(int[][] matrix) {
         this.n = matrix.length ; 
         this.m = matrix[0].length ; 
         int[][] count = new int[n][m] ; 
-        int[][] isVisited = new int[n][m] ; 
-
+        isVisited = new boolean[n][m] ; 
         for( int i=0 ; i<n ; ++i ){
             for( int j=0 ; j<m ; ++j ){
-                if( isVisited[i][j] == 0 ){
-                    dfs( matrix , isVisited , count , i , j ) ; 
+                if( !isVisited[i][j] ){
+                    rs = Math.max( rs , dfs( matrix , count , i , j  ) ) ; 
                 }
-            }
-        }
-        int rs = -1 ; 
-        for( int[] i : count  ){
-            for( int j : i ){
-                rs = Math.max( rs , j ) ; 
             }
         }
         return rs ; 
