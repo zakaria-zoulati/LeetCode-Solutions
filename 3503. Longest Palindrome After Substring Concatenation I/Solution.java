75f34aab9f10e9fb16162459f64a1d14e73f7e22
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-03-30 12:11:00 +0000

    A better Implementation

--- 3503. Longest Palindrome After Substring Concatenation I/Solution.java
+++ 3503. Longest Palindrome After Substring Concatenation I/Solution.java
@@ -1,33 +1,34 @@
 class Solution {
-    public boolean check( String s , int i , int j , String t , int a , int b ){
-        StringBuilder curr = new StringBuilder() ; 
-        curr.append( s.substring(i,j) ) ; 
-        curr.append( t.substring(a,b) ) ;
-        int len = curr.length() ; 
-        int l = 0 ; 
-        int r = len-1 ; 
-        while( l < r ){
-            if( curr.charAt(l) != curr.charAt(r) ) return false ; 
-            l++; 
-            r-- ; 
+    public boolean check(String s, int i, int j, String t, int a, int b) {
+        int len1 = j - i; 
+        int len2 = b - a; 
+        int len = len1 + len2; 
+        int l = 0, r = len - 1;
+        while (l < r) {
+            char x = (l < len1) ? s.charAt(i + l) : t.charAt(a + (l - len1));
+            char y = (r < len1) ? s.charAt(i + r) : t.charAt(a + (r - len1));
+            if (x != y) return false;
+            l++;
+            r--;
         }
-        return true ; 
+        return true;
     }
+
     public int longestPalindrome(String s, String t) {
-        int n = s.length() ; 
-        int m = t.length() ; 
-        int rs = 1 ; 
-        for( int i=0 ; i<=n ; ++i ){
-            for( int j=i ; j<=n ; ++j ){
-                for( int a=0 ; a<=m ; ++a ){
-                    for( int b=a ; b<=m ; ++b ){
-                        if( check( s , i , j , t , a , b ) ){
-                            rs = Math.max( rs , j-i + b-a ) ; 
+        int n = s.length();
+        int m = t.length();
+        int rs = 1;
+        for (int i = 0; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                for (int a = 0; a <= m; a++) {
+                    for (int b = a; b <= m; b++) {
+                        if (check(s, i, j, t, a, b)) {
+                            rs = Math.max(rs, (j - i) + (b - a)); 
                         }
                     }
                 }
             }
         }
-        return rs ;
+        return rs;
     }
 }
