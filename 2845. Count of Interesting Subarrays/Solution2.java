8f3f669f22e3d19c284ec30a2b5dbab29986ff59
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-04-25 17:38:47 +0000

    A better implementation

--- 2845. Count of Interesting Subarrays/Solution2.java
+++ 2845. Count of Interesting Subarrays/Solution2.java
@@ -1,3 +1,25 @@
-public class Solution2 {
-    
+import java.util.List;
+
+class Solution {
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+        int n = nums.size() ; 
+        if( k > n ){
+            return 0 ; 
+        }
+        long rs = 0 ; 
+        int count = 0 ; 
+        int[] map = new int[n+1] ; 
+        map[0] = 1 ; 
+        for( int i=0 ; i<n ; ++i ){
+            if( nums.get(i) % modulo == k ){
+                count = ( count + 1 ) % modulo ; 
+            }
+            int ele = ( count - k + modulo ) % modulo ; 
+            if( ele <= n ){
+                rs += map[ele] ; 
+            }
+            map[ count ]++ ; 
+        }
+        return rs ; 
+    }
 }
