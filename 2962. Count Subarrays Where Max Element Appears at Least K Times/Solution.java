180ebee43c284a8866dd21070cbb61b4ef344661
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-04-29 15:21:54 +0000

    Greedy Pb

--- 2962. Count Subarrays Where Max Element Appears at Least K Times/Solution.java
+++ 2962. Count Subarrays Where Max Element Appears at Least K Times/Solution.java
@@ -5,22 +5,27 @@ class Solution {
         int max = -1 ; 
         int count = 0 ; 
         int start = 0 ;  
+        int[] q = new int[n] ; 
+        int in = 0 ; 
+        int p = 0 ; 
         for( int i=0 ; i<n ; ++i ){
             if( nums[i] > max ){
                 start = i ; 
                 count = 1 ; 
                 max = nums[i] ; 
+                p = 0 ; 
+                in = 0 ; 
                 rs = 0 ; 
             }else if( nums[i] == max ){
+                q[in++] = i ; 
                 count++ ; 
             }
-            if( count >= k  ){
-                while( count > k ) {
-                    if( nums[start] == max ) count-- ; 
-                    start++ ; 
-                }
-                while( nums[start] != max ) start++ ; 
+            if( count > k ){
+                start = q[p++] ;  
+                count-- ; 
                 rs += start + 1 ; 
+            }else if( count == k ){
+                rs += start + 1; 
             }
         }
         return rs ; 
