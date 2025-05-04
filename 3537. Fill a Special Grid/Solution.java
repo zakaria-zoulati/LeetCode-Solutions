38e91f44d01cfee704d4f4ff490bc090d057c781
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-05-04 11:23:36 +0000

    More Readable Solution

--- 3537. Fill a Special Grid/Solution.java
+++ 3537. Fill a Special Grid/Solution.java
@@ -1,19 +1,21 @@
 class Solution {
-    public void fill( int[][] grid ,  int i ,int j , int f , int ele ){
+    int ele ; 
+    public void fill( int[][] grid ,  int i ,int j , int f  ){
         if( f == 1 ){
-            grid[i][j] = ele ; 
+            grid[i][j] = ele-- ; 
             return ; 
         } 
-        fill( grid , i , j , f/2 , ele ) ; 
-        fill( grid , i + f/2 ,j , f/2 , ele - f*f/4 ) ; 
-        fill(  grid , i+f/2 , j+f/2 , f/2 , ele - f*f/2  ) ; 
-        fill( grid , i , j +f/2 , f/2 , ele - 3*f*f/4  ) ; 
+        fill( grid , i , j , f/2) ; 
+        fill( grid , i + f/2 ,j , f/2 ) ; 
+        fill(  grid , i+f/2 , j+f/2 , f/2  ) ; 
+        fill( grid , i , j +f/2 , f/2   ) ; 
     }
     public int[][] specialGrid(int n) {
         int m = 1 << n ; 
         int[][] grid = new int[m][m] ; 
         if( n == 0 ) return grid ; 
-        fill( grid , 0 , 0 , m , m*m -1 ) ; 
+        ele = m*m - 1; 
+        fill( grid , 0 , 0 , m ) ; 
         return grid ; 
     }
 }
