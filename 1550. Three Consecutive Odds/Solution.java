71b9cd3f9f11d4c93bd18363051b2295c1d11c0c
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-05-11 20:14:55 +0000

    Implementation Pb

---
+++ 1550. Three Consecutive Odds/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+       int n = arr.length ; 
+       for( int i=0 ; i+2<n ; ++i ){
+            if( arr[i] % 2 == 1 && arr[i+1]%2==1 && arr[i+2]%2 ==1 ){
+                return true ; 
+            }
+       }
+       return false ; 
+    }
+}
