fb59776b9902ac9f2ee10dbaea8fb391153ced0e
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-05-26 20:26:32 +0000

    Topological Sort Pb

---
+++ 1857. Largest Color Value in a Directed Graph/Solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int largestPathValue(String colors, int[][] edges) {
+        char[] ch = colors.toCharArray() ; 
+        int n = colors.length();
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        int[] inDegree = new int[n] ; 
+        for (int i = 0; i < n; ++i) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] e : edges) {
+            int u = e[0] , v = e[1] ; 
+            graph[u].add(v);
+            inDegree[v]++ ; 
+        }        
+        Queue<Integer> q = new LinkedList<>() ; 
+        for( int i=0 ; i<n ; ++i ){
+            if( inDegree[i] == 0 ){
+                q.offer(i) ; 
+            }
+        }
+        int rs = 0 ; 
+        int[][] dp = new int[n][26] ;  
+        int vis = 0 ; 
+        while( !q.isEmpty() ){
+            int node = q.poll() ;
+            ++vis ; 
+            int curr = ch[node] - 'a' ; 
+            rs = Math.max( rs , ++dp[node][curr] ) ; 
+            for( int i : graph[node] ) {
+                for( int c=0; c<26 ; ++c ){
+                    dp[i][c] = Math.max( dp[i][c] , dp[node][c] ) ; 
+                }
+                if( --inDegree[i] == 0 ){
+                    q.offer( i ) ; 
+                }
+            }
+        }
+       return vis == n ? rs : -1 ; 
+    }
+}
