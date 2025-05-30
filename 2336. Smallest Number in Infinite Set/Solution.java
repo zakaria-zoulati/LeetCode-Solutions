2279f8639810c89e1ce238a7a05d10faf863efbe
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-05-30 15:46:14 +0000

    Design  Pb

---
+++ 2336. Smallest Number in Infinite Set/Solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    
+}class SmallestInfiniteSet {
+    boolean[] nums ; 
+    int in = 1 ; 
+    public SmallestInfiniteSet() {
+        nums = new boolean[1001] ;  
+        for( int i=1 ; i<=1000 ; ++i ) nums[i] = true; 
+    }
+    public int popSmallest() {
+        int rs = in ; 
+        nums[in] = false ; 
+        while( in < 1001 && !nums[in] ) in++ ; 
+        if( in == 1001 ) in = -1 ; 
+        return rs ; 
+        
+    }
+    public void addBack(int num) {
+        if( in == -1 ){
+            in = num ; 
+        }else if( num < in ){
+            in = num ;
+        }
+        nums[num] = true ; 
+    }
+}
+
