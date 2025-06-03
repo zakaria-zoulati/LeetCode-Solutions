b40ab98c9453de95186a68cbcbe2735703aab877
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-06-03 21:03:04 +0000

    BFS Pb

---
+++ 1298. Maximum Candies You Can Get from Boxes/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        int n = status.length ; 
+        boolean[] isSeen = new boolean[n] ; 
+        Queue<Integer> q = new ArrayDeque<>() ; 
+        int rs = 0 ; 
+        for( int curr : initialBoxes ){
+            isSeen[curr] = true ; 
+            if( status[curr] == 1 ){
+                q.add(curr) ; 
+                rs += candies[curr] ; 
+            }
+        }
+        while( !q.isEmpty() ){
+            int curr = q.poll() ;  
+            for( int i : keys[curr] ){
+                if( status[i] == 1 ) continue ; 
+                status[i] = 1 ; 
+                if( isSeen[i]  ){
+                    q.add(i) ; 
+                    rs += candies[i] ; 
+                }
+            }
+            for( int i : containedBoxes[curr] ){
+                if( isSeen[i] ) continue ; 
+                isSeen[i] = true ; 
+                if( status[i] == 1 ){
+                    q.add(i) ; 
+                    rs += candies[i] ; 
+                }
+            }
+        }
+        return rs ; 
+    }
+}
