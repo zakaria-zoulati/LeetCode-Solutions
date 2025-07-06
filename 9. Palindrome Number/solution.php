9e35efbde68912159a9991ee9f30407a58686c08
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-07-06 00:32:16 +0000

    some Pb

---
+++ 9. Palindrome Number/solution.php
@@ -0,0 +1,11 @@
+<?php
+    class Solution {
+        /**
+         * @param Integer $x
+         * @return Boolean
+         */
+        function isPalindrome($x) {
+            return strrev( $x ) == $x ; 
+        }
+    }
+?>
