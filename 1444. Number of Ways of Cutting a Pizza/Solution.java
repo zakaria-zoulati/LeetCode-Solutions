6a25f2330b2a0cb771467dc0d5ccbaacc408c094
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-08-07 20:46:09 +0100

    DP Pb

--- 1444. Number of Ways of Cutting a Pizza/Solution.java
+++ 1444. Number of Ways of Cutting a Pizza/Solution.java
@@ -1,48 +1,48 @@
 class Solution {
     public int ways(String[] pizza, int k) {
+        if( k == 1 ){
+            for( String s : pizza ){
+                for( char c : s.toCharArray() ){
+                    if( c == 'A' ) return 1 ; 
+                }
+            }
+            return 0 ; 
+        }   
         int n = pizza.length; 
         int m = pizza[0].length(); 
         int mod = 1_000_000_007; 
-
         int[][] count = new int[n + 1][m + 1]; 
         for (int i = n - 1; i >= 0; --i) {
             for (int j = m - 1; j >= 0; --j) {
                 count[i][j] = count[i][j + 1] + count[i + 1][j] - count[i + 1][j + 1]; 
-                if (pizza[i].charAt(j) == 'A') {
-                    count[i][j]++; 
-                }
+                if (pizza[i].charAt(j) == 'A') count[i][j]++ ; 
             }
-        }
-
-        if (k == 1) {
-            return count[0][0] > 0 ? 1 : 0; 
         }
         if (count[0][0] < k) return 0; 
-
         int[][][] dp = new int[n][m][k + 1]; 
-
         for (int i = n - 1; i >= 0; --i) {
             for (int j = m - 1; j >= 0; --j) {
-                if (count[i][j] > 0) {
+                int c = count[i][j] ; 
+                if (c > 0) {
                     dp[i][j][1] = 1; 
-                }
-                int c = count[i][j]; 
-                int curr = c<k ? c : k ; 
-                for (int t = 2; t <= curr; ++t) {
-                    for (int a = i + 1; a < n; ++a) {
-                        if (count[a][j] < c) {
-                            dp[i][j][t] = (dp[i][j][t] + dp[a][j][t - 1]) % mod; 
+                    int curr = c<k ? c : k ; 
+                    for (int t = 2; t <= curr; ++t) {
+                        for (int a = i + 1; a < n; ++a) {
+                            if (count[a][j] < c) {
+                                if( count[a][j] < t-1 ) break ; 
+                                dp[i][j][t] = (dp[i][j][t] + dp[a][j][t - 1]) % mod; 
+                            }
                         }
-                    }
-                    for (int a = j + 1; a < m; ++a) {
-                        if (count[i][a] < c) {
-                            dp[i][j][t] = (dp[i][j][t] + dp[i][a][t - 1]) % mod; 
+                        for (int a = j + 1; a < m; ++a) {
+                            if (count[i][a] < c) {
+                                if( count[i][a] < t-1 ) break ; 
+                                dp[i][j][t] = (dp[i][j][t] + dp[i][a][t - 1]) % mod; 
+                            }
                         }
                     }
                 }
             }
         }
-
         return dp[0][0][k]; 
     }
 }
