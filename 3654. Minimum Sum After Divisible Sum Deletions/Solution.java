ed37adf2d7a1e8e7174800f15578f272a215a6d4
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-08-17 14:39:55 +0100

    Clean Implementation

--- 3654. Minimum Sum After Divisible Sum Deletions/Solution.java
+++ 3654. Minimum Sum After Divisible Sum Deletions/Solution.java
@@ -1,26 +1,20 @@
 class Solution {
     public long minArraySum(int[] nums, int k) {
-        int n = nums.length ; 
-        long sum = 0 ; 
-        for(int i: nums){
-            sum += i ; 
-        }
+        int n = nums.length ;
         long[] dp = new long[n] ; 
         int curr = 0 ; 
         HashMap<Integer,Long> map = new HashMap<>(); 
         for( int i=0 ; i<n ; ++i ){
             curr = ( curr + nums[i] ) % k ; 
-            dp[i] = Long.MAX_VALUE ; 
             if( curr == 0 ){
                 dp[i] = 0 ; 
             }  
             else if( map.get(curr) != null ){
-                dp[i] = map.get(curr) ; 
+                dp[i] = Math.min( map.get(curr) , (i>0?dp[i-1]:0) + nums[i] );
+            }else {
+                dp[i] = (i>0?dp[i-1]:0) + nums[i]  ; 
             }
-            dp[i] = Math.min( dp[i] , (i>0?dp[i-1]:0) + nums[i] ) ; 
-            if( map.get(curr) == null || map.get(curr) > dp[i] ){
-                map.put(curr,dp[i]) ; 
-            }
+            map.put(curr,dp[i]) ; 
         }
         return dp[n-1] ; 
     }
