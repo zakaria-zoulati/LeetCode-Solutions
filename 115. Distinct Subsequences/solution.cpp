ecd8e57a0de36a6b3eab3b11ad0f66c2e1ea82bd
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-08-20 03:38:44 +0100

    TYPO

--- 115. Distinct Subsequences/solution.cpp
+++ 115. Distinct Subsequences/solution.cpp
@@ -8,7 +8,7 @@ class Solution {
             vector<ll> dp(m+1,0) ; 
             dp[0] = 1 ; 
             for( int i=1 ; i<=n ; ++i ){
-                for( int j=m ; j>0. ; --j ){
+                for( int j=m ; j>0 ; --j ){
                     if( s[i-1] == t[j-1] ){
                         dp[j] += dp[j-1] ; 
                     }
