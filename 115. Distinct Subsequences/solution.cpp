c3b59143e79f12d44d0c462cc084bc78ce10a6cd
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-08-20 03:37:37 +0100

    DP PB

---
+++ 115. Distinct Subsequences/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        using ll = unsigned long long ; 
+        int numDistinct(string s, string t) {
+            int n = s.size() ; 
+            int m = t.size() ; 
+            if( m > n ) return 0 ; 
+            vector<ll> dp(m+1,0) ; 
+            dp[0] = 1 ; 
+            for( int i=1 ; i<=n ; ++i ){
+                for( int j=m ; j>0. ; --j ){
+                    if( s[i-1] == t[j-1] ){
+                        dp[j] += dp[j-1] ; 
+                    }
+                }
+            }
+            ll rs = dp[m] ; 
+            return (int) rs ; 
+        }
+    };
