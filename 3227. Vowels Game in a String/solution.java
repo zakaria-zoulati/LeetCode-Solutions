150d1eb9af2eec89a2f0347430e617bd83a293c8
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-09-12 01:47:31 +0100

    Better Implementation

--- 3227. Vowels Game in a String/solution.java
+++ 3227. Vowels Game in a String/solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        int vowels = 0 ; 
         for( char c : s.toCharArray() ){
-            if( c=='a' || c=='e' || c=='i' || c=='a' || c=='o' || c=='u' ) vowels++ ; 
-        }
-        if(vowels!=0 ){
-            return true ;
+            switch(c){
+                case 'a' , 'e' , 'u' , 'i' , 'o' : 
+                    return true  ;
+            }
         }
         return false ; 
     }
