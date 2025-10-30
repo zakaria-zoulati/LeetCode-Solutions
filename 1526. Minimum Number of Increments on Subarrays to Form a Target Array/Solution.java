48631759cc8a8e8161ecaab97fc3bdef087baf0b
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-10-30 01:16:00 +0100

    Daily Pb

---
+++ 1526. Minimum Number of Increments on Subarrays to Form a Target Array/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int n = target.length ; 
+        int ans =  target[0] ; 
+        for( int i=1 ; i<n ; ++i ){
+            if( target[i] > target[i-1] ){
+                ans += target[i] - target[i-1] ; 
+            }
+        } 
+        return ans ; 
+    }
+}
