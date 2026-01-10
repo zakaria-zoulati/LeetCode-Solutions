872d6356fc5dfe81c76e6a03d4fef184657d7190
zakaria-zoulati <zoulatizakaria3@gmail.com>
2026-01-10 02:47:00 +0100

    Daily Pb

--- 712. Minimum ASCII Delete Sum for Two Strings/Solution.java
+++ 712. Minimum ASCII Delete Sum for Two Strings/Solution.java
@@ -1,26 +1,25 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
+        int[] b1 = s1.chars().toArray() ; 
+        int[] b2 = s2.chars().toArray() ; 
         int n = s1.length() ; 
         int m = s2.length() ; 
         int[][] dp = new int[n+1][m+1] ; 
         for( int i=0 ; i<m ; ++i ){
-            dp[0][i+1] = dp[0][i] + ( (int) s2.charAt(i) ) ; 
+            dp[0][i+1] = dp[0][i] + b2[i] ; 
         }
-
         for( int i=0 ; i<n ; ++i ){
-            dp[i+1][0] = dp[i][0] + ( (int) s1.charAt(i) ) ; 
+            dp[i+1][0] = dp[i][0] + b1[i] ; 
         }
-
         for( int i=0 ; i<n ; ++i ){
             for( int j=0 ; j<m ; ++j ){
-                if( s1.charAt(i) == s2.charAt(j) ){
+                if( b1[i] == b2[j] ){
                     dp[i+1][j+1] = dp[i][j] ; 
                 }else {
-                    dp[i+1][j+1] = Math.min( dp[i+1][j] + ( (int) s2.charAt(j) ) , dp[i][j+1] + ( (int) s1.charAt(i)  )  ) ; 
+                    dp[i+1][j+1] = Math.min( dp[i+1][j] + b2[j] , dp[i][j+1] + b1[i] ) ; 
                 }
             }
         }
-
         return dp[n][m] ; 
     }
 }
