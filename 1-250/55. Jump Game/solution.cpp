af9a96dda3763606b2e90bf991fbc22ea1d70116
VarunArora31 <varuna.cs.23@nitj.ac.in>
2026-01-14 16:53:09 +0530

    Added C++ Sol for LC55

---
+++ 1-250/55. Jump Game/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        // I am thinking in the direction that 0 in nums array is the main problem
+        // if i am at index where nums[i] = 0 or can reach at index before that index where nums[i] = 0
+        // that i cannot reach last index return false
+        int mx_jump = 0;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            mx_jump = max(mx_jump, i + nums[i]);
+            if (nums[i] == 0 && mx_jump <= i) return false;
+        }
+        return true;
+    }
+};
