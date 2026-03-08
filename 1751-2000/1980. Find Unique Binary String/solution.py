646814d10d2ea5d64eb41890dea244da0c3362f9
zakaria-zoulati <zoulatizakaria3@gmail.com>
2026-03-08 21:23:20 +0000

    A Better solution

--- 1751-2000/1980. Find Unique Binary String/solution.py
+++ 1751-2000/1980. Find Unique Binary String/solution.py
@@ -1,22 +1,10 @@
 from typing import List
-
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        def helper( num , n ) : 
-            s = "" 
-            for i in range( n ) : 
-                if num % 2 == 0 : 
-                    s = "0" + s  
-                else : 
-                    s = "1" + s
-                num >>= 1 
-            return s
-        n = len(nums)
-        for i in range(1<<n) : 
-            flag = True 
-            for num in nums : 
-                if int( num , 2 ) == i : 
-                    flag = False 
-                    break 
-            if flag : 
-                return helper( i , n )
+        res = []
+        for i in range(len(nums)): 
+            if nums[i][i] == '1' : 
+                res.append( '0' )
+            else : 
+                res.append( '1' )
+        return "".join(res)
