f13e8c8a52267d0927e49a1fdbbf855c9a7963b1
zakaria-zoulati <zoulatizakaria3@gmail.com>
2026-03-30 21:00:15 +0100

    A better solution

--- 3751-4000/2840. Check if Strings Can be Made Equal With Operations II/Solution.java
+++ 3751-4000/2840. Check if Strings Can be Made Equal With Operations II/Solution.java
@@ -2,20 +2,20 @@ class Solution {
     public boolean checkStrings(String s1, String s2) {
         int n = s1.length() ; 
         int[] f1 = new int[26] ; 
-        int[] f2 = new int[26] ;
-        for( int i=0 ; i<n ; ++i ){
-            if( i % 2 == 0 ){
-                f1[ s2.charAt(i) - 'a' ]-- ; 
-                f1[ s1.charAt(i) - 'a' ]++ ; 
-            }else {
-                f2[ s1.charAt(i) - 'a' ]++ ; 
-                f2[ s2.charAt(i) - 'a' ]-- ; 
-            }
+        for( int i=0 ; i<n ; i += 2 ){
+            f1[ s2.charAt(i) - 'a' ]-- ; 
+            f1[ s1.charAt(i) - 'a' ]++ ; 
         } 
         for( int i=0 ; i<26 ; ++i ){
-            if( f1[i] != 0 || f2[i] != 0 ){
-                return false ; 
-            }
+            if( f1[i] != 0 ) return false ; 
+        }
+        int[] f2 = new int[26] ;
+        for( int i=1 ; i<n ; i += 2 ){
+            f2[ s2.charAt(i) - 'a' ]-- ; 
+            f2[ s1.charAt(i) - 'a' ]++ ; 
+        } 
+        for( int i=0 ; i<26 ; ++i ){
+            if( f2[i] != 0 ) return false ; 
         }
         return true ; 
     }
